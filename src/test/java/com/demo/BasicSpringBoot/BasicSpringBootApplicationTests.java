f4608c658fb69671aae106b7374852404910dd94
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-10 23:30:49 +0100

    Initial commit

---
+++ src/test/java/com/demo/BasicSpringBoot/BasicSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.BasicSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
